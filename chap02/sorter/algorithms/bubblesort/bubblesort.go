554d28d74cd9eb75b2054ba107b593eeee98c4c3
Soli <soli@cbug.org>
2015-03-16 00:02:05 +0800

    bubble sort;

--- chap02/sorter/algorithms/bubblesort/bubblesort.go
+++ chap02/sorter/algorithms/bubblesort/bubblesort.go
@@ -1,5 +1,19 @@
 package bubblesort
 
-func Sort(vals []int) (err error) {
-	return
+func Sort(vals []int) {
+
+	for i := len(vals) - 1; i > 0; i-- {
+		jump := true
+
+		for j := 0; j < i; j++ {
+			if vals[j] > vals[j+1] {
+				vals[j], vals[j+1] = vals[j+1], vals[j]
+				jump = false
+			}
+		}
+
+		if jump {
+			break
+		}
+	}
 }
--- chap02/sorter/algorithms/bubblesort/bubblesort_test.go
+++ chap02/sorter/algorithms/bubblesort/bubblesort_test.go
@@ -1,1 +1,49 @@
 package bubblesort
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func assert(vals []int, exp []int, e *testing.T) {
+	Sort(vals)
+
+	fmt.Println("result is:", vals)
+
+	if !reflect.DeepEqual(vals, exp) {
+		e.Error("\nresult is:", vals, "\nexpect is:", exp)
+	}
+}
+
+func TestQSort0(e *testing.T) {
+	assert ([]int{1, 2, 3, 4, 5},
+			[]int{1, 2, 3, 4, 5}, e)
+}
+
+func TestQSort1(e *testing.T) {
+	assert ([]int{5, 4, 3, 2, 1},
+			[]int{1, 2, 3, 4, 5}, e)
+}
+
+func TestQSort2(e *testing.T) {
+	assert ([]int{5, 4, 5, 2, 1},
+			[]int{1, 2, 4, 5, 5}, e)
+}
+
+
+func TestQSort3(e *testing.T) {
+	assert ([]int{5, 4},
+			[]int{4, 5}, e)
+}
+
+func TestQSort4(e *testing.T) {
+	assert ([]int{5},
+			[]int{5}, e)
+}
+
+func TestQSort5(e *testing.T) {
+	assert ([]int{},
+			[]int{}, e)
+}
+
